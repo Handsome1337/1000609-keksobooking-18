b5cb5b001f75e3a97bea0947401e843def523a40
Ilya Volkov <volkov-ilyha@ya.ru>
2019-11-01 00:08:01 +0300

    Личный проект: большое нефильтрованное

--- js/filter.js
+++ js/filter.js
@@ -3,13 +3,40 @@
 (function () {
   /* Максимальное количество отображаемых меток */
   var MAX_PINS_QUANTITY = 5;
+  var DEBOUNCE_DURATION = 500; // полсекунды
 
   var filtersForm = document.querySelector('.map__filters');
   var disabledFilters = filtersForm.querySelectorAll('[disabled]');
   var housingType = filtersForm.querySelector('#housing-type');
+  var housingPrice = filtersForm.querySelector('#housing-price');
+  var housingRoom = filtersForm.querySelector('#housing-rooms');
+  var housingGuest = filtersForm.querySelector('#housing-guests');
+  var activeHousingFeatures = null;
+  var lastTimeout = null;
 
+  /* Переходные цены в фильтре цены */
+  var Price = {
+    INFINITY: Infinity,
+    TOP: 50000,
+    MIDDLE: 10000,
+    BOTTOM: 0
+  };
+
+  /* Активирует/деактивирует фильтры */
   var changeFiltersStatus = function (flag) {
     window.util.changeDisabledAttr(disabledFilters, flag);
+    if (flag) {
+      filtersForm.reset();
+    }
+  };
+
+  /* Сравнивает цену объявления с минимальным и максимальным значениями фильтра по цене */
+  var comparePrice = function (price, min, max) {
+    if (price >= min && price <= max) {
+      return price;
+    } else {
+      return false;
+    }
   };
 
   /* Фильтрует количество отображаемых элементов на карте */
@@ -28,13 +55,72 @@
     });
   };
 
+  /* Фильтрует по цене */
+  var filterHousingPrice = function (offers) {
+    return offers.filter(function (offer) {
+      if (housingPrice.value === 'any') {
+        return offer;
+      } else if (housingPrice.value === 'middle') {
+        return comparePrice(offer.offer.price, Price.MIDDLE, Price.TOP);
+      } else if (housingPrice.value === 'low') {
+        return comparePrice(offer.offer.price, Price.BOTTOM, Price.MIDDLE);
+      } else {
+        return comparePrice(offer.offer.price, Price.TOP, Price.INFINITY);
+      }
+    });
+  };
+
+  /* Фильтрует по количеству комнат */
+  var filterHousingRooms = function (offers) {
+    return offers.filter(function (offer) {
+      if (housingRoom.value === 'any') {
+        return offer;
+      } else {
+        return offer.offer.rooms === parseInt(housingRoom.value, 10);
+      }
+    });
+  };
+
+  /* Фильтрует по количеству гостей */
+  var filterHousingGuests = function (offers) {
+    return offers.filter(function (offer) {
+      if (housingGuest.value === 'any') {
+        return offer;
+      } else {
+        return offer.offer.guests === parseInt(housingGuest.value, 10);
+      }
+    });
+  };
+
+  /* Фильтрует по доступным опциям */
+  var filterFeatures = function (offers) {
+    activeHousingFeatures = Array.from(filtersForm.querySelectorAll('.map__checkbox:checked')).map(function (feature) {
+      return feature.value;
+    });
+    return offers.filter(function (offer) {
+      return activeHousingFeatures.every(function (feature) {
+        return offer.offer.features.indexOf(feature) !== -1;
+      });
+    });
+  };
+
+  /* Фильтрует объявления по всем фильтрам */
+  var filterPosts = function (posts) {
+    return filterQuantity(filterHousingGuests(filterHousingRooms(filterFeatures(filterHousingPrice(filterHousingType(posts))))));
+  };
+
   /* Заполняет карту в соответствии со всеми фильтрами */
   var setFilterOffersCallback = function (data, callback) {
-    var offers = data.slice();
     filtersForm.addEventListener('change', function () {
-      callback(filterQuantity(filterHousingType(offers)));
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(function () {
+        callback(filterPosts(data));
+      }, DEBOUNCE_DURATION);
     });
-    callback(filterQuantity(filterHousingType(offers)));
+    callback(filterPosts(data));
+    changeFiltersStatus(false);
   };
 
   window.filter = {
